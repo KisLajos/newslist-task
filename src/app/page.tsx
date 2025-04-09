c81d6c8e868e402f2017f7d0f2b702f1edadeb76
KisLajos <64649246+KisLajos@users.noreply.github.com>
2025-04-09 15:38:37 +0200

    Added NewListComponent to Home()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import NewsListComponent from "@/components/news-list";
 
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8 text-center">Latest News</h1>
+        <NewsListComponent />
       </main>
   );
 }
